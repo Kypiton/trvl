76c51275dbd1e2f030022433a4892fe98f355c51
Dan <partytimeyep@gmail.com>
2023-07-14 16:19:47 +0300

    commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,26 @@
+import Navbar from '../Navbar/Navbar';
+import Home from '../pages/Home';
+import Services from '../pages/Services';
+import Products from '../pages/Products';
+import SignUp from '../pages/SignUp';
+import './App.css';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/services' component={Services} />
+                    <Route path='/products' component={Products} />
+                    <Route path='/sign-up' component={SignUp} />
+                </Switch>
+            </Router>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react';
+
+import img from '../../assets/images/img-9.jpg'
+
+import CardItem from "../CardItem/CardItem";
+
+import './Cards.css';
+
+const Cards = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await fetch('http://localhost:3001/cardsWith2Images');
+                const response = await result.json();
+                setData(response);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchData();
+    }, []);
+    
+    return (
+        <div className='cards'>
+            <h1>Check out these EPIC Destinations!</h1>
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <ul className="cards__items">
+                        {data.map(item => (
+                            <CardItem
+                                src={item.src}
+                                text={item.text}
+                                label={item.label}
+                                path={item.path}
+                                key={item.id} />
+                        ))}
+                    </ul>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src='images/img-3.jpg'
+                            text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+                            label='Mystery'
+                            path='/services' />
+                        <CardItem
+                            // src='images/img-4.jpg'
+                            text='Experience Football on Top of the Himilayan Mountains'
+                            label='Adventure'
+                            path='/products' />
+                        <CardItem
+                            // src='images/img-8.jpg'
+                            text='Ride through the Sahara Desert on a guided camel tour'
+                            label='Adrenaline'
+                            path='/sign-up' />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards;
